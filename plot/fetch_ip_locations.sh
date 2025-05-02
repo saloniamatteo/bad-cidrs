7900b235b32b8e6f6cfd760c086148d1475e3666
saloniamatteo <saloniamatteo@pm.me>
2025-05-02 17:03:25 +0200

    Use local geoip instead of external service

--- plot/fetch_ip_locations.sh
+++ plot/fetch_ip_locations.sh
@@ -17,43 +17,19 @@ CIDRSFILE="${DIRNAME}/../CIDRs.txt"
 # Only get IPs (strip owner/company name)
 awk '{print $1}' "${CIDRSFILE}" > "${DIRNAME}/cidrs-oneline.txt"
 
-# Create empty data array
-data_array=()
-
-# Create empty points array
-points=()
-
-makerequest() {
-    # Convert data accordingly
-    array_string=$(printf "[%s]" "$(IFS=,; echo "${data_array[*]}")")
-
-    # Get location data
-    # Sample response:
-    # [{"lat":0,"lon":0},{"lat":0,"lon":0}]
-    points+=$(curl "http://ip-api.com/batch?fields=lat,lon" --data "${array_string}" 2>/dev/null)
-
-    # Flush data_array
-    data_array=()
-}
-
 # Read file
 while IFS= read -r line; do
     # Get base IP
     base_ip=$(sipcalc "${line}" | awk '/Network address/ {print $4}')
 
-    # If array size is equal to 100, send batch request
-    if [ ${#data_array[@]} -eq 100 ]; then
-        makerequest
-    else
-        # Add base IP to array
-        data_array+=("\"${base_ip}\"")
+    # Geoip lookup
+    latlon=$(geoiplookup $base_ip | grep "City Edition")
+    lat=$(echo $latlon | cut -d',' -f7)
+    lon=$(echo $latlon | cut -d',' -f8)
+
+    # If points are valid (i.e. they are not empty),
+    # then append them to points.txt.
+    if ! [[ -z "$lat" && -z "$lon" ]]; then
+        echo "$lon,$lat" >> "${DIRNAME}/points.txt"
     fi
 done < "${DIRNAME}/cidrs-oneline.txt"
-
-# Check if data_array is empty: if it isn't, complete pending tasks
-if ! [ -z $data_array ]; then
-    makerequest
-fi
-
-# Parse response and save it into our points file
-jq -r '.[] | "\(.lon),\(.lat)"' <<<$points > "${DIRNAME}/points.txt"
